da1e07a60fc82413f8d2fb00c2d3b40bdd3bacf4
Florian Dütsch <mail@florian-duetsch.de>
2018-03-07 20:36:31 +0100

    Hammerspoon: Cycled hyper keys

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,22 +6,22 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 --------------------------------------------------------------------------------
--- Präfix für alle Fenster-bezogenen Kommandos
+-- Präfix für die meisten bzw. restlichen Hammerspoon-Kommandos
 -- Per Karabiner auf rechtes ⌘ gebunden
 local hyper = {"⌘", "⌥", "⌃"}
 
--- Präfix für die meisten bzw. restlichen Hammerspoon-Kommandos
+-- Präfix für alle Fenster-bezogenen Kommandos
 -- Per Karabiner auf rechtes ⌥ gebunden
 local hyper2 = {"⌘", "⌥", "⌃", "⇧"}
 
 --------------------------------------------------------------------------------
-hs.hotkey.bind(hyper2, "c", require "new_chrome_window")
-hs.hotkey.bind(hyper2, "t", require "new_iterm_window")
-hs.hotkey.bind(hyper2, "i", require "toggle_ksheet")
+hs.hotkey.bind(hyper, "c", require "new_chrome_window")
+hs.hotkey.bind(hyper, "t", require "new_iterm_window")
+hs.hotkey.bind(hyper, "i", require "toggle_ksheet")
 
 -- Karabiner Elements setzt aktuell leider "⌃-eject" außer Kraft.
 -- https://github.com/tekezo/Karabiner-Elements/issues/89
-hs.hotkey.bind(hyper2, "s", hs.caffeinate.systemSleep)
+hs.hotkey.bind(hyper, "s", hs.caffeinate.systemSleep)
 
 --------------------------------------------------------------------------------
 -- Fenster-bezogene Kommandos
@@ -29,7 +29,7 @@ hs.hotkey.bind(hyper2, "s", hs.caffeinate.systemSleep)
 -- Die Animationen sind laggy, also schalten wir sie ab.
 hs.window.animationDuration = 0
 
-hs.hotkey.bind(hyper, "f", function()
+hs.hotkey.bind(hyper2, "f", function()
   hs.mjomatic.go({
     "iiiCCCCC",
     "iiiCCCCC",
@@ -39,7 +39,7 @@ hs.hotkey.bind(hyper, "f", function()
   })
 end)
 
-hs.hotkey.bind(hyper, "g", function()
+hs.hotkey.bind(hyper2, "g", function()
   hs.mjomatic.go({
     "iiiaaaaa",
     "iiiaaaaa",
@@ -57,13 +57,13 @@ hs.loadSpoon("WindowHalfsAndThirds")
 -- https://github.com/miromannino/miro-windows-manager
 -- http://www.hammerspoon.org/Spoons/
 local windowHotkeys = {
-  left_half = { hyper, "a" },
-  right_half = { hyper, "d" },
-  undo = { hyper, "r" },
-  center = { hyper, "s" },
-  max_toggle = { hyper, "w" },
-  larger = { hyper, "+" },
-  smaller = { hyper, "-" }
+  left_half = { hyper2, "a" },
+  right_half = { hyper2, "d" },
+  undo = { hyper2, "r" },
+  center = { hyper2, "s" },
+  max_toggle = { hyper2, "w" },
+  larger = { hyper2, "+" },
+  smaller = { hyper2, "-" }
 }
 -- TODO: 40/60 interessant? 30/70? Auf welche Tasten?
 -- TODO: max + undo statt max_toggle?
