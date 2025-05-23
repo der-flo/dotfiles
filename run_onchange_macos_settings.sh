fd2853d53920c4fa3d26eb916fd0618909777248
Florian Dütsch <florian.duetsch@adigi.ai>
2025-05-23 12:19:09 +0200

    macOS setting regarding multiple spaces

--- run_onchange_macos_settings.sh
+++ run_onchange_macos_settings.sh
@@ -28,3 +28,5 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# TODO: "Alle Eingabequellen: Punkt mit doppeltem Leerzeichen hinzufügen" deaktivieren
